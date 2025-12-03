361f07fa6d5a4fc4bdea439517bcfb69f20fc2b3
fpfcmsr <fpf@andrew.cmu.edu>
2025-12-03 23:06:19 +0000

    Create netbird-repo.sh

---
+++ files/scripts/netbird-repo.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+tee /etc/yum.repos.d/netbird.repo <<EOF
+[netbird]
+name=netbird
+baseurl=https://pkgs.netbird.io/yum/
+enabled=1
+gpgcheck=0
+gpgkey=https://pkgs.netbird.io/yum/repodata/repomd.xml.key
+repo_gpgcheck=1
+EOF
