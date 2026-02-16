0f286e83eac500f0fb07945e90d9e2a9a468447f
fpfcmsr <fpf@andrew.cmu.edu>
2026-02-16 15:45:40 -0800

    noctalia

--- files/scripts/zed-editor.sh
+++ files/scripts/zed-editor.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf5 -y config-manager setopt terra.enabled=1
-dnf5 -y install zed
+dnf5 -y install zed noctalia-shell
 dnf5 -y config-manager setopt terra.enabled=0
