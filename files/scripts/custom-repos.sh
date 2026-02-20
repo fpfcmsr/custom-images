0993a36c3e017cb1872c49e0528f57a22afbfe55
fpfcmsr <fpf@andrew.cmu.edu>
2026-02-19 17:09:18 -0800

    Update and rename terra.sh to custom-repos.sh

--- files/scripts/custom-repos.sh
+++ files/scripts/custom-repos.sh
@@ -6,3 +6,7 @@ dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/te
 dnf5 -y config-manager setopt terra.enabled=1
 dnf5 -y install zed noctalia-shell
 dnf5 -y config-manager setopt terra.enabled=0
+
+
+dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paul4us/Fedora_43/home:paul4us.repo
+dnf5 -y install klassy
