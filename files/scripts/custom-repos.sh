a3ae6d7fa06e87c2c4ced09be6777a26f894da20
fpfcmsr <fpf@andrew.cmu.edu>
2026-02-20 09:32:07 -0800

    copr -y

--- files/scripts/custom-repos.sh
+++ files/scripts/custom-repos.sh
@@ -11,12 +11,12 @@ dnf5 -y config-manager setopt terra.enabled=0
 dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paul4us/Fedora_43/home:paul4us.repo
 dnf5 -y install klassy
 
-dnf copr enable bazzite-org/webapp-manager fedora-43-x86_64
-dnf copr enable jsbillings/netbird fedora-43-x86_64
-dnf copr enable bazzite-org/bazzite fedora-43-x86_64
+dnf copr enable -y bazzite-org/webapp-manager fedora-43-x86_64
+dnf copr enable -y jsbillings/netbird fedora-43-x86_64
+dnf copr enable -y bazzite-org/bazzite fedora-43-x86_64
 
 dnf5 -y install webapp-manager steamdeck-kde-presets-desktop netbird-client
 
-dnf copr disable bazzite-org/webapp-manager fedora-43-x86_64
-dnf copr disable jsbillings/netbird fedora-43-x86_64
-dnf copr disable bazzite-org/bazzite fedora-43-x86_64
+dnf copr disable -y bazzite-org/webapp-manager fedora-43-x86_64
+dnf copr disable -y jsbillings/netbird fedora-43-x86_64
+dnf copr disable -y bazzite-org/bazzite fedora-43-x86_64
