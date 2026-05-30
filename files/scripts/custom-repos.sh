0762899cf191a61ef61c8c0ff97126a5e3e59841
fpfcmsr <fpf@andrew.cmu.edu>
2026-05-30 02:41:31 -0700

    editor changes

--- files/scripts/custom-repos.sh
+++ files/scripts/custom-repos.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf5 -y config-manager setopt terra.enabled=1
-dnf5 -y install zed
+dnf5 -y install zed vscodium
 dnf5 -y config-manager setopt terra.enabled=0
-
+dnf5 -y remove code
 
 #dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:paul4us/Fedora_43/home:paul4us.repo
 #dnf5 -y install klassy
@@ -14,20 +14,14 @@ dnf5 -y config-manager setopt terra.enabled=0
 dnf config-manager addrepo --from-repofile=https://download.opensuse.org/repositories/home:luisbocanegra/Fedora_43/home:luisbocanegra.repo
 dnf5 -y install plasma-panel-colorizer
 
-dnf copr enable -y bazzite-org/webapp-manager fedora-43-x86_64
+#dnf copr enable -y bazzite-org/webapp-manager fedora-43-x86_64
 #dnf copr enable -y jsbillings/netbird fedora-43-x86_64
 dnf copr enable -y bazzite-org/bazzite fedora-43-x86_64
 
-dnf5 -y install webapp-manager steamdeck-kde-presets-desktop # netbird-client
+dnf5 -y install steamdeck-kde-presets-desktop # netbird-client webapp-manager
 
-dnf copr disable -y bazzite-org/webapp-manager
+#dnf copr disable -y bazzite-org/webapp-manager
 #dnf copr disable -y jsbillings/netbird
 dnf copr disable -y bazzite-org/bazzite
 
 # dnf5 -y install texlive*
-
-#dnf5 -y install niri niri-settings noctalia-shell
-
-#dnf5 -y remove xdg-desktop-gnome gnome-desktop3 gnome-desktop4
-
-# dnf install @development-tools -y
