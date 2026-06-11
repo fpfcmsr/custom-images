172e388f14cd2eef529d448e2ca41ee138f58b29
fpfcmsr <fpf@andrew.cmu.edu>
2026-06-11 13:03:04 -0700

    key add

--- files/scripts/custom-repos.sh
+++ files/scripts/custom-repos.sh
@@ -26,6 +26,8 @@ dnf copr disable -y bazzite-org/bazzite
 
 
 dnf config-manager addrepo --from-repofile="https://codeberg.org/api/packages/GramEditor/rpm.repo"
+rpm --import https://codeberg.org/api/packages/GramEditor/rpm/repository.key
+
 dnf -y install gram
 
 
